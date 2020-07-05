adb8c492da46bd4e2af07c2f6e7e01b6daeb6679
RKCP <rkcpeters@gmail.com>
2020-07-05 09:47:16 +0100

    Added scan and batchGet for read operations.

--- aws-sdk-js-testing/read-operations.js
+++ aws-sdk-js-testing/read-operations.js
@@ -7,7 +7,7 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 docClient.get({
     TableName: 'testDatabase_sdk',
 
-    // Give the key of the row we wish to update. 
+    // Give the primary key of the row we wish to get. 
     // In this case, both the user_id and timestamp combine as the key.
     Key: {
         user_id: '1000',
@@ -43,3 +43,24 @@ docClient.query({
 
 // After querying, can use FilterExpression to filter specific details, like id > 10 or something.
 // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.04.html
+
+
+/** This function lets us scan/query rows across all the partitions in the DynamoDB table
+ *  It scans before it filters.
+ */
+docClient.scan({
+    TableName: 'testDatabase_sdk',
+    FilterExpression: "title = :title", // filter on the title
+    ExpressionAttributeValues: { // where title = 'test title'
+        ":title" : 'test title'
+    }
+}, (err, data) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+})
+
+
+// There is also a .batchGet operation which allows you to request multiple items from multiple tables at once.
